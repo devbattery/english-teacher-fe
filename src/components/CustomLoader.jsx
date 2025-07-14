80c79208809fd10ab491b353a238fde4692d7d02
devbattery <devbattery@outlook.com>
2025-07-14 14:22:19 +0900

    Style: 로딩 애니메이션 변경 후 영역 삭제

--- src/components/CustomLoader.jsx
+++ src/components/CustomLoader.jsx
@@ -5,7 +5,11 @@ import './CustomLoader.css';
 const CustomLoader = ({ message }) => {
   return (
     <div className="loader-container">
-      <div className="spinner"></div>
+      <div className="bouncing-loader">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
       <p className="loader-message">{message}</p>
     </div>
   );

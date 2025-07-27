8eac7edecd2909ab19a64039fe8c3847eb76934b
devbattery <devbattery@outlook.com>
2025-07-27 19:18:22 +0900

    Style: 단어장 아이콘 변경

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -26,6 +26,14 @@ const MoonIcon = () => (
   </svg>
 );
 
+const BookIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+    <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+  </svg>
+);
+
+
 const LogoutIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/>
@@ -73,8 +81,9 @@ const NavigationBar = () => {
           </div>
         ) : user ? (
           <>
-            <Link to="/vocabulary" className="navbar-button vocab-link">
-              내 단어장
+            {/* --- [변경] 텍스트 버튼에서 아이콘 링크로 변경 --- */}
+            <Link to="/vocabulary" className="vocab-link" title="내 단어장">
+              <BookIcon />
             </Link>
 
             <div className="navbar-user" ref={dropdownRef}>
